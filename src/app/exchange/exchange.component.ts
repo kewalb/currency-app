ac0918f49e82d419ea4ff5f3fc61b5c3879fcfb2
kewalb <bkewal@gmail.com>
2023-09-25 13:12:05 +0530

    Minor changes

--- src/app/exchange/exchange.component.ts
+++ src/app/exchange/exchange.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from '../../environment/environment';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-exchange',
